6c11324b4dd4cda1e3afbbfcf120eb3938da69dc
ctmworkin <ctmworkin@WIN-WORKSTATION>
2022-03-11 14:32:10 -0600

    Add project files.

---
+++ Appy McASPNETface/Controllers/ExercisesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Appy_McASPNETface.Controllers
+{
+    public class ExercisesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("~/Views/Exercises/BeginnerExercise");
+        }
+
+        public IActionResult BeginnerExercise()
+        {
+            return View();
+        }
+
+        public IActionResult IntermediateExercise()
+        {
+            return View();
+        }
+        public IActionResult AdvancedExercise()
+        {
+            return View();
+        }
+    }
+}
---
+++ Appy McASPNETface/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Appy_McASPNETface.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
